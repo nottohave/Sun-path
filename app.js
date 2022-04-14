61a05d95e3aac8e5da44bb6c804e600c97053868
nottohave <nottohave@gmail.com>
2022-04-14 01:29:43 -0700

    app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            square.setAttribute('draggable', true)
+            square.setAttribute('id', i)
             let randomColor = Math.floor(Math.random() * candyColors.length)
             square.style.backgroundColor = candyColors[randomColor]
             grid.appendChild(square)
@@ -24,6 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     createBoard()
+
+    // Drag the candies
+    squares.forEach(square => square.addEventListener('dragstart', dragStart))
+    squares.forEach(square => square.addEventListener('dragend', dragStart))
+    squares.forEach(square => square.addEventListener('dragover', dragStart))
+    squares.forEach(square => square.addEventListener('dragenter', dragStart))
+    squares.forEach(square => square.addEventListener('dragleave', dragStart))
+    squares.forEach(square => square.addEventListener('drop', dragStart))
+
 })
 
 // think
